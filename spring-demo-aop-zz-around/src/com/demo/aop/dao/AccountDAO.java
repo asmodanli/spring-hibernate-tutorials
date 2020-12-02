27b08926ce5f7fa2507a290cd497d5c0f5d950df
Sena <a.modanlioglu@ogr.iu.edu.tr>
2020-12-02 14:17:57 +0300

    calculating the delay with around advice

---
+++ spring-demo-aop-zz-around/src/com/demo/aop/dao/AccountDAO.java
@@ -0,0 +1,72 @@
+package com.demo.aop.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.stereotype.Component;
+
+import com.demo.aop.Account;
+
+@Component
+public class AccountDAO {
+	
+	private String name;
+	private String serviceCode;
+	
+	// add a new method - > findAccounts()
+
+	public List<Account> findAccounts(boolean tripWire) {
+		
+		if(tripWire) {
+			throw new RuntimeException("throwing an exception");
+		}
+		
+		List<Account> myAccounts = new ArrayList<Account>();
+		
+		// create sample accounts
+		Account temp1 = new Account("john", "silver");
+		Account temp2 = new Account("sena", "gold");
+		Account temp3 = new Account("ayse", "platinium");
+
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+			
+		return myAccounts;
+	}
+	
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		System.out.println(getClass() + "DOING DB WORK: ADD ACCOUNT");
+	}
+	
+	public boolean doWork() {
+		System.out.println(getClass() + "DOING SOME WORK: DAO");
+		return true;
+	
+	}
+
+	public String getName() {
+		System.out.println(getClass() + " getname");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + " setname");
+
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + "getservicecode");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + "setservicecode");
+		this.serviceCode = serviceCode;
+	}
+	
+	
+}
