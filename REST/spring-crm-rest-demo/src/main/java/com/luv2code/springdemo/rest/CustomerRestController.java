dabd17d4f6b8550b0608dc216ffb7ef25a7fdbb4
Sena <a.modanlioglu@ogr.iu.edu.tr>
2020-12-14 03:48:00 +0300

    insert operations

--- REST/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ REST/spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,7 +40,15 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 	
-	
+	// add mapping for POST / customers
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) { // get json data as pojo
+		
+		theCustomer.setId(0); // 0 makes create new id(insert) 
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
 	
 	
 }
