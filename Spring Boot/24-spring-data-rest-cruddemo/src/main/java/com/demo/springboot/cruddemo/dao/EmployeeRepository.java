f417b537eb7b1475d26085e567fa04e3c923d4dd
Sena <a.modanlioglu@ogr.iu.edu.tr>
2020-12-18 21:26:07 +0300

    configuration, pagination, sorting

--- Spring Boot/24-spring-data-rest-cruddemo/src/main/java/com/demo/springboot/cruddemo/dao/EmployeeRepository.java
+++ Spring Boot/24-spring-data-rest-cruddemo/src/main/java/com/demo/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.demo.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.demo.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
